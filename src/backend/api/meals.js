1b1d9a61cacee39951a8eda85d7f9bf56cee02f4
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-03-14 19:49:40 +0100

    install

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,1 +1,15 @@
-const express = require("express");
+import express from "express";
+const router = express.Router();
+import knex from "../database.js";
+
+router.get("/", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const titles = await knex("meals").select("title");
+    response.json(titles);
+  } catch (error) {
+    throw error;
+  }
+});
+
+export default router;
--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,18 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import mealsRouter from "./api/meals.js";
+import cors from "cors";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const router = express.Router();
-const path = require("path");
 
-const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
-const port = process.env.PORT || 3000;
-const cors = require("cors");
-const knex = require("./database");
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -22,73 +27,6 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
-// Respond with all meals in the future (relative to the when datetime)
-app.get("/future-meals", async(req, res) => {
-  try {
-    const futureMeals = await knex("Meal")
-      .select()
-      .where('when', '>', new Date());
-    if (futureMeals.length === 0) {
-      res.status(404).send("The future meals is empty");
-      return;
-    }
-    res.status(200).json(futureMeals);
-  } catch(error) {
-      console.error(error);
-      res.status(500).send("Something went wrong");
-  }
-});
-
-// Respond with all meals in the past (relative to the when datetime)
-app.get("/past-meals", async (req, res) => {
-  try {
-    const date = new Date();
-    const pastMeals = await knex("Meal")
-      .select()
-      .where("when", "<", date);
-    res.status(200).json(pastMeals);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
-  }
-});
-
-//Respond with all meals sorted by ID
-app.get("/all-meals", async (req, res) => {
-  try {
-    const allMeals = await knex("Meal")
-      .select()
-      .orderBy('id');
-    res.status(200).json(allMeals);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
-  }
-});
-
-//	Respond with the first meal (meaning with the minimum id)
-app.get("/first-meal", async (req, res) => {
-  try {
-    const firstMeal = await knex("Meal").select().orderBy("id").first();
-    res.status(200).json(firstMeal);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
-  }
-});
-
-//	Respond with the last meal (meaning with the maximum id)
-app.get("/last-meal", async (req, res) => {
-  try {
-    const lastMeal = await knex("Meal").select().orderBy("id", 'desc').first();
-    res.status(200).json(lastMeal);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
-  }
-});
-
-
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
@@ -100,4 +38,4 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
 
-module.exports = app;
+export default app;
