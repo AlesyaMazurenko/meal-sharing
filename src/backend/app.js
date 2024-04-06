ebbeec0ba9712285661a3961b34e9bb4c8a197a2
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-06 14:17:55 +0200

    Update app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,6 +4,8 @@ import mealsRouter from "./api/meals.js";
 import cors from "cors";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import knex from "knex";
+import { error } from "console";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +15,7 @@ const app = express();
 const router = express.Router();
 
 const buildPath = path.join(__dirname, "../../dist");
+const port = process.env.PORT || 3000;
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -36,6 +39,68 @@ if (process.env.API_PATH) {
 // for the frontend. Will first be covered in the react class
 app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
+});
+
+// Respond with all meals in the future (relative to the when datetime)
+app.get("/future-meals", async (req, res) => {
+  try {
+    const futureMeals = await knex("Meal")
+      .select()
+      .where("when", ">", new Date());
+    if (futureMeals.length === 0) {
+      res.status(404).send("The future meals is empty");
+      return;
+    }
+    res.status(200).json(futureMeals);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// Respond with all meals in the past (relative to the when datetime)
+app.get("/past-meals", async (req, res) => {
+  try {
+    const date = new Date();
+    const pastMeals = await knex("Meal").select().where("when", "<", date);
+    res.status(200).json(pastMeals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//Respond with all meals sorted by ID
+app.get("/all-meals", async (req, res) => {
+  try {
+    const allMeals = await knex("Meal").select().orderBy("id");
+    res.status(200).json(allMeals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//	Respond with the first meal (meaning with the minimum id)
+app.get("/first-meal", async (req, res) => {
+  try {
+    const firstMeal = await knex("Meal").select().orderBy("id").first();
+    res.status(200).json(firstMeal);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//	Respond with the last meal (meaning with the maximum id)
+app.get("/last-meal", async (req, res) => {
+  try {
+    const lastMeal = await knex("Meal").select().orderBy("id", "desc").first();
+    res.status(200).json(lastMeal);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 export default app;
