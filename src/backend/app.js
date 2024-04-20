4e36b26f294c88ea76c48ba980c0c8d6c9c0599f
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-20 17:11:40 +0200

    Update app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -104,4 +104,72 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
 
+<<<<<<< Updated upstream
 module.exports = app;
+=======
+// Respond with all meals in the future (relative to the when datetime)
+app.get("/future-meals", async (req, res) => {
+  try {
+    const futureMeals = await knex("Meal")
+      .select()
+      .where("when", ">", new Date());
+    if (futureMeals.length === 0) {
+      res.status(404).send("The future meals is empty");
+      return;
+    }
+    res.status(200).json(futureMeals);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// Respond with all meals in the past (relative to the when datetime)
+app.get("/past-meals", async (req, res) => {
+  try {
+    const date = new Date();
+    const pastMeals = await knex("Meal").select().where("when", "<", date);
+    res.status(200).json(pastMeals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//Respond with all meals sorted by ID
+app.get("/all-meals", async (req, res) => {
+  try {
+    const allMeals = await knex("Meal")
+      .select()
+      .orderBy('id');
+    res.status(200).json(allMeals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//	Respond with the first meal (meaning with the minimum id)
+app.get("/first-meal", async (req, res) => {
+  try {
+    const firstMeal = await knex("Meal").select().orderBy("id").first();
+    res.status(200).json(firstMeal);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//	Respond with the last meal (meaning with the maximum id)
+app.get("/last-meal", async (req, res) => {
+  try {
+    const lastMeal = await knex("Meal").select().orderBy("id", "desc").first();
+    res.status(200).json(lastMeal);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+export default app;
+>>>>>>> Stashed changes
