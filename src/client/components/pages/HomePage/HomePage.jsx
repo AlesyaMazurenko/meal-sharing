2ded2f2e8a5abb1391a098267fc101ee3c0e5273
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-27 10:33:30 +0200

    Update HomePage.jsx

--- src/client/components/pages/HomePage/HomePage.jsx
+++ src/client/components/pages/HomePage/HomePage.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
+import Header from "../../Header/Header.jsx";
+import { AppBar } from "../../Appbar/Appbar.jsx";
 // import { fetchPopularMovie } from "api/Api";
 // import MoviesItem from "components/MoviesItem/MoviesItem";
 
 export default function HomePage() {
-  const [trending, setTrending] = useState();
+  // const [trending, setTrending] = useState();
 
-  useEffect(() => {
-    async function getTrendingMovies() {
-      try {
-        const data = await fetchPopularMovie();
-        setTrending(data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getTrendingMovies();
-  }, []);
+  // useEffect(() => {
+  //   async function getTrendingMovies() {
+  //     try {
+  //       const data = await fetchPopularMovie();
+  //       setTrending(data.results);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   getTrendingMovies();
+  // }, []);
 
   return (
     <main>
-      <h1>Home Page</h1>
-      <h2>Trending films today</h2>
-      <ul className="meals-list">
-        {trending && <MoviesItem data={trending} />}
-      </ul>
+     
+      <AppBar />
+      <Header />
+   
     </main>
   );
 }
