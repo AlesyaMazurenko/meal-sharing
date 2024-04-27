f8f31dbcbe50bce0368fc62a9ffbfbe5c4478cce
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-27 10:33:41 +0200

    Update MealItem.jsx

--- src/client/components/pages/MealsList/MealItem.jsx
+++ src/client/components/pages/MealsList/MealItem.jsx
@@ -3,6 +3,7 @@ function MealItem({ meal }) {
     return (
         <div className="meal_card">
             <h3>{meal.title}</h3>
+            <img src={meal.img} alt={meal.title} width='460px'></img>
             <p>{meal.description}</p>
             <p>Price: {meal.price} kr.</p>
         </div>
