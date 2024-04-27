f9c12955a250c14d94c844337a60c9234c1534e8
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-27 10:32:34 +0200

    Update App.js

--- src/client/App.js
+++ src/client/App.js
@@ -4,26 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout.jsx";
 import TestComponent from "./components/TestComponent/TestComponent.js";
 import MealsList from "./components/pages/MealsList/MealsList.jsx";
-
+import NotFoundPage from "./components/pages/NotFoundPage/NotFound.jsx";
+import HomePage from "./components/pages/HomePage/HomePage.jsx";
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
-        <Layout/>
+        <HomePage />
         {/* <p>main</p> */}
         {/* <Meals List /> */}
       </Route>
       <Route exact path="/meals">
-        <p>test</p>
         <MealsList />
       </Route>
-      <Route exact path="/lol">
+      <Route path="*" element={<NotFoundPage />} />
+      {/* <Route exact path="/lol">
         <p>lol</p>
       </Route>
       <Route exact path="/test-component">
         <TestComponent></TestComponent>
-      </Route>
+      </Route> */}
     </Router>
   );
 }
