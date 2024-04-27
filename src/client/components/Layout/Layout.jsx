36383a71ed91f21ae7bf499237f25b6687c1d369
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-27 10:33:11 +0200

    Update Layout.jsx

--- src/client/components/Layout/Layout.jsx
+++ src/client/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Layout.css';
 import { AppBar } from '../Appbar/Appbar';
 
+
 export const Layout = () => {
     return (
         <>
-            <header className='navbar'>
+            <div className='navbar'>
+        
                 <AppBar />
-            </header>  
+            </div>  
         </>
     )
 }
