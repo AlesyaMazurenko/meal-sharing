b6b5654e120e820f6b1810797ca5ae2fde908233
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-27 20:12:01 +0200

    Update HomePage.jsx

--- src/client/components/pages/HomePage/HomePage.jsx
+++ src/client/components/pages/HomePage/HomePage.jsx
@@ -1,24 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Home.css";
 import Header from "../../Header/Header.jsx";
 import { AppBar } from "../../Appbar/Appbar.jsx";
-// import { fetchPopularMovie } from "api/Api";
-// import MoviesItem from "components/MoviesItem/MoviesItem";
 
 export default function HomePage() {
-  // const [trending, setTrending] = useState();
-
-  // useEffect(() => {
-  //   async function getTrendingMovies() {
-  //     try {
-  //       const data = await fetchPopularMovie();
-  //       setTrending(data.results);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   getTrendingMovies();
-  // }, []);
 
   return (
     <main>
