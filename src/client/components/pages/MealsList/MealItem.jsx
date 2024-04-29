ec5ddc8195c52e5ceffe40adaf7931717e790a43
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-04-29 20:51:34 +0200

    Update MealItem.jsx

--- src/client/components/pages/MealsList/MealItem.jsx
+++ src/client/components/pages/MealsList/MealItem.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function MealItem({ meal }) {
     return (
-        <div className="meal_card">
-            <h3>{meal.title}</h3>
-            <img src={meal.img} alt={meal.title} width='460px'></img>
-            <p>{meal.description}</p>
-            <p>Price: {meal.price} kr.</p>
-        </div>
-    )
+      <div className="meal_card">
+        <Link to={`/meals/${meal.id}`}>
+          <h3>{meal.title}</h3>
+          {/* <img src={meal.img} alt={meal.title} width="460px"></img> */}
+          <p>{meal.description}</p>
+          <p>Price: {meal.price} kr.</p>
+        </Link>
+      </div>
+    );
 }
 
 export default MealItem;
