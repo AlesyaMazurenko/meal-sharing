955f201e1a71ec8d24952b7449b760f464322a76
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-07 23:17:37 +0200

    Update MealItem.jsx

--- src/client/components/pages/MealsList/MealItem.jsx
+++ src/client/components/pages/MealsList/MealItem.jsx
@@ -5,7 +5,7 @@ function MealItem({ meal }) {
       <div className="meal_card">
         <Link to={`/meals/${meal.id}`}>
           <h3>{meal.title}</h3>
-          {/* <img src={meal.img} alt={meal.title} width="460px"></img> */}
+          <img src={meal.img} alt={meal.title} width="460px"></img>
           <p>{meal.description}</p>
           <p>Price: {meal.price} kr.</p>
         </Link>
