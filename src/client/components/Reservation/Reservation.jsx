7929501634808a469176e56437362cfad538be08
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-10 15:59:40 +0200

    Create Reservation.jsx

---
+++ src/client/components/Reservation/Reservation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router";
+
+export default function Reservation() {
+    return (
+      <div>
+        <p> Reservation form</p>
+      </div>
+    );
+}
