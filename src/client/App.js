901101eca644068a78f4c98fa80e11957dc84971
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-10 16:00:02 +0200

    Update App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,32 +1,68 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter, Routes, Route, Switch} from "react-router-dom";
 
 import { Layout } from "./components/Layout/Layout.jsx";
 import TestComponent from "./components/TestComponent/TestComponent.js";
 import MealsList from "./components/pages/MealsList/MealsList.jsx";
-import NotFoundPage from "./components/pages/NotFoundPage/NotFound.jsx";
 import HomePage from "./components/pages/HomePage/HomePage.jsx";
+import NotFoundPage from "./components/pages/NotFoundPage/NotFound.jsx";
+import MealInfo from "./components/MealInfo/MealInfo.jsx";
+import Reviews from "./components/Reviews/Reviews.jsx";
+import Header from "./components/Header/Header.jsx";
+import { AppBar } from "./components/Appbar/Appbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Reservation from "./components/Reservation/Reservation.jsx";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/">
-        <HomePage />
-        {/* <p>main</p> */}
-        {/* <Meals List /> */}
-      </Route>
-      <Route exact path="/meals">
-        <MealsList />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-      {/* <Route exact path="/lol">
+    <div>
+      <AppBar />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route exact path="/meals">
+          <MealsList />
+        </Route>
+
+        <Route exact path="/meals/:id">
+          <MealInfo />
+          <Route  path="reviews">
+            <Reviews />
+          </Route>
+          <Route path="reservation">
+            <Reservation/>
+          </Route>
+        </Route>
+
+        {/* <Route path="reservation" element={<Reservation />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
+        {/* <Route exact path="/lol">
         <p>lol</p>
       </Route>
       <Route exact path="/test-component">
         <TestComponent></TestComponent>
       </Route> */}
-    </Router>
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+
+//  <Routes>
+//           <Route exact path="/">
+//             <HomePage />
+//           </Route>
+//           <Route exact path="/meals">
+//             <MealsList />
+//           </Route>
+//           <Route path="/meals/:id">
+//             <MealInfo />
+//             <Route path="/reviews">
+//               <Reviews />
+//             </Route>
