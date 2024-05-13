d19780063d54c34d9501121f17eae8ecd33c10de
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-13 23:20:32 +0200

    Update HomePage.jsx

--- src/client/components/pages/HomePage/HomePage.jsx
+++ src/client/components/pages/HomePage/HomePage.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Home.css";
-
-import { AppBar } from "../../Appbar/Appbar.jsx";
 import Main from "../../Main/Main.jsx";
 
 export default function HomePage() {
 
   return (
     <main>
-     
-      {/* <AppBar /> */}
+
       <Main />
    
     </main>
