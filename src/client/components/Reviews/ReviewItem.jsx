f35dd35ffbc49fcc83f73eaab47d07c015fff693
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-13 23:20:44 +0200

    Update ReviewItem.jsx

--- src/client/components/Reviews/ReviewItem.jsx
+++ src/client/components/Reviews/ReviewItem.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { nanoid } from "nanoid";
+import './reviewItem.css';
 
 export default function ReviewItem({ data }) {
     console.log(data);
-    return data.map(({ title, description,stars }) => {
+    return data.map(({ title, description, stars }) => {
       return (
-        <li className="reviews-item" key={nanoid()}>
+          <li className="reviews-item" key={nanoid()}>
+              <span className="line"></span>
               <h4>{title}</h4>
               <p>Rating {stars}/10</p>
               <p>{description}</p>
