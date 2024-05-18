e5ef478f3da31aed4f0382785d4f8e5d21480a46
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-05-18 21:48:29 +0200

    Update ReviewItem.jsx

--- src/client/components/Reviews/ReviewItem.jsx
+++ src/client/components/Reviews/ReviewItem.jsx
@@ -6,11 +6,31 @@ export default function ReviewItem({ data }) {
     console.log(data);
     return data.map(({ title, description, stars }) => {
       return (
-          <li className="reviews-item" key={nanoid()}>
-              <span className="line"></span>
-              <h4>{title}</h4>
-              <p>Rating {stars}/10</p>
-              <p>{description}</p>
+        <li className="reviews-item" key={nanoid()}>
+          <span className="line"></span>
+          <h4>{title}</h4>
+          <p>Rating {stars}/5</p>
+          <span
+            className="fa fa-star"
+            style={{ color: Number(stars) >= 1 ? "orange" : "grey" }}
+          ></span>
+          <span
+            className="fa fa-star"
+            style={{ color: Number(stars) >= 2 ? "orange" : "grey" }}
+          ></span>
+          <span
+            className="fa fa-star"
+            style={{ color: Number(stars) >= 3 ? "orange" : "grey" }}
+          ></span>
+          <span
+            className="fa fa-star"
+            style={{ color: Number(stars) >= 4 ? "orange" : "grey" }}
+          ></span>
+          <span
+            className="fa fa-star"
+            style={{ color: Number(stars) >= 5 ? "orange" : "grey" }}
+          ></span>
+          <p>{description}</p>
         </li>
       );
     });
